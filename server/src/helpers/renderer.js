56feeed13e78d7f9e4b461f6f1cd06848fcf6a52
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-11-27 21:10:24 +0530

    Removed Build Folders

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -6,15 +6,15 @@ export default () => {
   const content = renderToString(<Home />);
 
   return `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>SSR React</title>
-    </head>
-    <body>
-      <div id="root">${content}</div>
-    </body>
-    <script src="bundle.js"></script>
-  </html>`;
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SSR React</title>
+  </head>
+  <body>
+    <div id="root">${content}</div>
+  </body>
+  <script src="bundle.js"></script>
+</html>`;
 };
