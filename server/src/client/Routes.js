34ff7c54059fe6fc244f9b304167de18d25ba5ed
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-11-28 13:57:16 +0530

    Added Redux

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import UsersList from "./components/UsersList";
 
 export default () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/users" element={<UsersList />} />
     </Routes>
   );
 };
--- server/src/client/client.js
+++ server/src/client/client.js
@@ -2,10 +2,18 @@ import React from "react";
 import { hydrateRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { thunk } from "redux-thunk";
+import reducers from "./reducers";
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 hydrateRoot(
   document.getElementById("root"),
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
 );
